c70bf686ad6fad4958c390604cfb7de8ee2a0c8d
BENMP0902 <benjamin2401128@hybridge.education>
2026-07-09 23:06:45 -0600

    feat: add CLI entrypoint for password generator

---
+++ main.py
@@ -0,0 +1,26 @@
+import sys
+from generator.password_generator import generate_password
+
+def main():
+
+    if len(sys.argv) < 2:
+        print("Uso : python main.py <longitud>")
+        return
+    
+    try:
+        length = int(sys.argv[1])
+    except ValueError:
+        print("Error: la longitud deber ser un número")
+        return
+    
+    if length < 4:
+        print("Error: la longitud minima es 4")
+        return
+    
+    password = generate_password(length)
+
+    print("Contraseña generada:", password)
+
+if __name__ == "__main__":
+    main()
+
